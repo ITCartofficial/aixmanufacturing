fafe439e2515836e96b470f3f39e5ca6d67f6db1
numandotdev <numandotdev@gmail.com>
2025-04-23 16:33:46 +0530

    Add SemiDonut chart component and update Dashboard layout

--- src/components/common/Container.jsx
+++ src/components/common/Container.jsx
@@ -1,14 +1,18 @@
 import React, { Children } from "react";
 import SelectDropdown from "./SelectDropdown";
+import { Link } from "react-router";
+import { MdChevronRight } from "react-icons/md";
+
 
 const Container = ({ id, label, dropDown, children, opt, className = "" }) => {
   return (
     <div
       id={id}
-      className={`w-full border h-max grow bg-[#ffffff] rounded-[10px] p-6 flex flex-col gap-6 ${className}`}
+      className={`w-full border h-full grow bg-[#ffffff] rounded-[10px] p-6 flex flex-col gap-6 ${className}`}
     >
       <div className="card_header flex items-center justify-between w-100">
         <h2 className="text-[20px] font-bold">{label}</h2>
+        <div className="flex items-center gap-4">
         {dropDown && (
           <SelectDropdown
             data={[
@@ -17,7 +21,15 @@ const Container = ({ id, label, dropDown, children, opt, className = "" }) => {
             ]}
           />
         )}
-        {opt && opt}
+        {opt && (
+          <Link to={opt.path} className="flex items-center w-full">
+            <button className="text-blue-500 hover:text-blue-700 text-[16px] font-medium">
+              {opt.label}
+            </button>
+            <MdChevronRight className="text-blue-500 hover:text-blue-700 text-[16px]" />
+          </Link>
+        )}
+        </div>
       </div>
       {children}
     </div>
