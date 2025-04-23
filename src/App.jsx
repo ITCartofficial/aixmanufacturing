52637bc2df00ba16f95d9dc1518faddf669f47d3
rezwanhussain <rezwanhussain80@gmail.com>
2025-04-23 19:15:30 +0530

    Add new icons, enhance StatCard and TitleBar components, and implement Pagination and Table components
    - Added new icons: alert.png, settings-green.png, and settings-purple.png.
    - Enhanced StatCard component to include status indicators with color mapping and improved layout.
    - Modified TitleBar component to accept dynamic title and button props.
    - Introduced Pagination component for managing pagination logic and UI.
    - Created Table component for displaying plant data with sorting and row selection functionality.
    - Integrated plants data into the Plants page, utilizing the new Table and Pagination components.
    - Updated CSS to fix hidden label styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
+import Machines from "./pages/Machines";
 import Plants from "./pages/Plants";
 import { Routes, Route } from "react-router";
 
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/plants" element={<Plants />} />
+        <Route path="/machines" element={<Machines />} />
       </Routes>
     </MainLayout>
   );
--- src/components/common/TitleBar.jsx
+++ src/components/common/TitleBar.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import {FiUpload} from "react-icons/fi";
-import PrimaryBtn from "../buttons/PrimaryBtn";
 
-const TitleBar = () => {
+const TitleBar = ({title, outlineBtn, fillBtn}) => {
   return (
-    <section className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center pt-4 sm:pt-6 gap-4">
+    <section className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
       <div>
         <h1 className="text-2xl sm:text-3xl font-semibold text-black">
-          Plants
+          {title}
         </h1>
       </div>
       <div className="flex flex-col sm:flex-row gap-3 w-full sm:w-auto">
-        <button className="py-2 px-6 sm:px-10 border border-primary rounded-[10px] text-base text-primary font-medium w-full sm:w-auto hover:bg-primary-hover hover:text-white">
-          + Add New
-        </button>
-        <button className="flex items-center justify-center gap-2 px-6 sm:px-10 py-3 border border-primary rounded-[10px] text-base text-white font-medium bg-primary w-full sm:w-auto hover:bg-primary-hover">
-          Export <FiUpload size={20} />
-        </button>
+        {outlineBtn && (
+          <button className="py-2 px-6 sm:px-10 border border-primary rounded-[10px] text-base text-primary font-medium w-full sm:w-auto hover:bg-primary-hover hover:text-white">
+            {outlineBtn}
+          </button>
+        )}
+        {fillBtn && (
+          <button className="flex items-center justify-center gap-2 px-6 sm:px-10 py-3 border border-primary rounded-[10px] text-base text-white font-medium bg-primary w-full sm:w-auto hover:bg-primary-hover">
+            {fillBtn} <FiUpload size={20} />
+          </button>
+        )}
       </div>
     </section>
   );
